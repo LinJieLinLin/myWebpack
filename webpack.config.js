2d600ca86774aab15d902a142a43efe5b10608f0
linjielinlin <993353454@qq.com>
2017-05-04 00:56:24 +0800

    u -1

--- webpack.config.js
+++ webpack.config.js
@@ -51,28 +51,22 @@ module.exports = {
             //html模板加载器，可以处理引用的静态资源，默认配置参数attrs=img:src，处理图片的src引用的资源
             //比如你配置，attrs=img:src img:data-src就可以一并处理data-src引用的资源了，就像下面这样
             test: /\.html$/,
-            loader: "html-loader?attrs=img:src img:data-src"
+            use: "html-loader?attrs=img:src img:data-src"
         }, {
             //文件加载器，处理文件静态资源
             test: /\.(woff|woff2|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-            loader: 'file-loader?name=./fonts/[name].[ext]'
+            use: 'file-loader?name=./fonts/[name].[ext]'
         }, {
             //图片加载器，雷同file-loader，更适合图片，可以将较小的图片转成base64，减少http请求
             //如下配置，将小于8192byte的图片转成base64码
             test: /\.(png|jpg|gif)$/,
-            loader: 'url-loader?limit=18192&name=./img/[hash].[ext]'
+            use: 'url-loader?limit=18192&name=./img/[hash].[ext]'
         }]
     },
     plugins: [
         // 加载jq
         new webpack.ProvidePlugin({
             $: 'jquery'
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            // 将公共模块提取，生成名为`vendors`的chunk
-            name: 'jquery',
-            //提取哪些模块共有的部分
-            chunks: ['jquery']
         }),
         new webpack.optimize.CommonsChunkPlugin({
             // 将公共模块提取，生成名为`vendors`的chunk
